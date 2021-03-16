7f32935fc8a610e1cc7749c3fc9c304bec026dc3
Szymon Leszkiewicz <szymon.leszkiewicz10@gmail.com>
2021-03-17 00:21:17 +0100

    "2/1.2 adding black edges with 1 sek delay works check 1"

--- projektowanie/Lista 2/1.2.py
+++ projektowanie/Lista 2/1.2.py
@@ -16,16 +16,6 @@ G = nx.Graph()
 colors = nx.get_edge_attributes(G, 'color').values()
 fig, ax = plt.subplots(figsize=(6, 4))
 
-
-def ani(p):
-    x = [(random.randint(0, 100), random.randint(0, 100)) for x in range(node_numb)]
-    v = {}
-    for i in range(1, node_numb+1):
-        v[i] = list(x[i])
-        G.add_node(i)
-    nx.draw(G, v, edge_color=colors)
-    nx.draw_networkx_labels(G, v)
-
 v = {}
 i = 1
 
@@ -33,26 +23,21 @@ def nodes(num):
     global i
     x = (random.randint(0, 100), random.randint(0, 100))
     global v
+    global G
     v[i] = list(x)
     G.add_node(i)
     i += 1
-    nx.draw(G, v, edge_color=colors)
+    nx.draw(G, v)
     nx.draw_networkx_labels(G, v)
 
 j = 2
 def edges1(num):
-    global j
-    global v
+    global j, v, G
     colors = nx.get_edge_attributes(G, 'color').values()
-
-    G.add_edge(1, j, color = 'black')
-    nx.draw(G, v, edge_color=colors)
-
+    G.add_edge(1, j)
+    nx.draw(G, v, edge_color = 'r')
     nx.draw_networkx_labels(G, v)
     j += 1
-    print("halo")
-
-    nx.draw_networkx_labels(G, v)
 
 ani = FuncAnimation(fig, nodes, frames=node_numb-1, interval=50, repeat = False)
 
@@ -61,9 +46,6 @@ fig, ax = plt.subplots(figsize=(6, 4))
 
 an = FuncAnimation(fig, edges1, frames=node_numb-2, interval=50, repeat = False)
 plt.show()
-
-'''for i in range(2, node_numb+1):
-    G.add_edge(1, i, color = 'black')
 
 for i in range(2, node_numb + 1 ):
     min_dis = 99999
@@ -78,9 +60,10 @@ for i in range(2, node_numb + 1 ):
             min_dis = dis
             min_id = y
     #print("wynik: ", i, "   ", min_id)
-    G.add_edge(i, min_id, color='r')
+    G.add_edge(i, min_id,edge_color=colors)
 
 nx.draw(G, v, edge_color=colors)
+nx.draw_networkx_edges(G, v)
 nx.draw_networkx_labels(G, v)
 plt.show()
-'''
+
