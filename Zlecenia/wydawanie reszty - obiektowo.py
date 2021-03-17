6e17667a511076da8b92e6aa75de7f66c6950e92
Szymon Leszkiewicz <szymon.leszkiewicz10@gmail.com>
2021-03-17 23:24:21 +0100

    "zlcenia/wydawanie done"

--- Zlecenia/wydawanie reszty - obiektowo.py
+++ Zlecenia/wydawanie reszty - obiektowo.py
@@ -1,3 +1,62 @@
+'''
+zlotowki = [1, 2, 5, 10, 20, 50, 100, 200, 500]
+grosze = [1, 2, 5, 10, 20, 50]
+a = input("podaj wartość reszty do wydania: \nnp. 2.57")
+a = a.split('.')
+a = list(map(int, a))
+i=len(zlotowki)-1
+print(a)
+reszta = []
+while a[0] != 0:
+    if a[0] > zlotowki[i]:
+        a[0] -= zlotowki[i]
+        reszta.append(zlotowki[i])
+    else:
+        i-=1
+    print(reszta)'''
 
 
-a = float(input("podaj wartość reszty do wydania: "))
+class Reszta:
+
+    def wczytywanie_kwoty(self):
+
+        kwota = input('Reszta do wydania: \nnp. 2.56 lub 2.0').split('.')
+        x = int(kwota[0])
+        y = int(kwota[1])
+        self.x = x
+        self.y = y
+
+    def wydawanie(self):
+        zl = [500, 200, 100, 50, 20, 10, 5, 2, 1]
+        gr = [50, 20, 10, 5, 2, 1]
+
+        reszta_zl = [0] * len(zl)
+        reszta_gr = [0] * len(gr)
+        a = self.x
+        b = self.y
+        w1 = a
+        w2 = b
+        for id, c in enumerate(zl):
+            reszta_zl[id], w1 = divmod(w1, c)
+
+        for id, c in enumerate(gr):
+            reszta_gr[id], w2 = divmod(w2, c)
+
+        for kwota, c in zip(reszta_zl, zl):
+            if c > 5 and kwota:
+                print(kwota, "banknot/y", "->", c, "zl")
+            if kwota:
+                print(kwota, "monata/ty", "->", c, "zl")
+
+        for kwota, c in zip(reszta_gr, gr):
+            if kwota:
+                print(kwota, "monata/ty", "->", c, "gr")
+
+
+def main():
+    kwota = Reszta()
+    kwota.wczytywanie_kwoty()
+    kwota.wydawanie()
+
+if __name__ == "__main__":
+    main()
