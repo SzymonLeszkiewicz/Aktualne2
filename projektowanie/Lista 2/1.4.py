5d81caa8c138ea73648a22688f32d1a369e30c21
Szymon Leszkiewicz <szymon.leszkiewicz10@gmail.com>
2021-03-22 16:53:30 +0100

    "2/1.4 - finish 1.0"

--- projektowanie/Lista 2/1.4.py
+++ projektowanie/Lista 2/1.4.py
@@ -2,14 +2,17 @@ import networkx as nx
 import networkx.drawing as nxd
 import matplotlib.pyplot as plt
 import numpy as np
-from time import  sleep
+from time import sleep
 import math
 from itertools import count
 from numpy.random import uniform
 import random
+random.seed(115)
 from matplotlib.animation import FuncAnimation
 
-L = ['ap','ob']
+
+
+L = ['ap', 'ob', 'asd']
 # l = int(input("Podaj licznosc zbioru L: "))
 # L = []
 # for i in range(l):
@@ -23,44 +26,77 @@ G.add_edge('A', 'C')
 G.add_edge('C', 'D')
 G.add_edge('E', 'D')
 G.add_edge('F', 'D')
-nx.set_node_attributes(G, 'ttt', "usluga")
 
-#print('A' in G.neighbors('B'))
-#G.remove_node('D')
+# G.add_edge('F', 'A')
+# G.add_edge('E', 'B')
+# G.add_edge('E', 'A')
+
+pos = nx.circular_layout(G)
+nx.draw_networkx_nodes(G, pos, node_size=3000)
+nx.draw_networkx_labels(G, pos)
+nx.draw_networkx_edges(G, pos)
+plt.show()
+
+nx.set_node_attributes(G, '', "usluga")
+
 def usl(a, b):
-    # print(type(list(a.nodes)))
-    # print(list(a.nodes))
     g = random.choice(list(a.nodes))
-    sasiedzi = list(a.neighbors(g))
-    atrybutysa=[]
-    for i in sasiedzi:
-        atrybutysa.append(a.nodes[i]['usluga'])
-        #atrybutysa.append()
-    print(atrybutysa)
+    a.nodes[g]['usluga'] = L[0]
+    c = a.copy()
+    j = 1
+    for i in list(a.nodes):
+        if i == g:
+            continue
+        sasiedzi = list(a.neighbors(i))
+        atrybutysa = []
+        for x in sasiedzi:
+            atrybutysa.append(a.nodes[x]['usluga'])
+        if L[j] in atrybutysa:
+            j += 1
+            if j>= len(L):
+                print("tak tego nie da sie zrobic")
+                c.remove_node(g)
+                return usl(c, 0)
+        a.nodes[i]['usluga'] = L[j]
+
+        # for x in L:
+        #    # a.nodes[x]['usluga'] = L[0]
+        #     for i in list(a.nodes):
+        #         if a.nodes[i]['usluga'] in atrybutysa:
+        #             print('zle')
+
+    # atrybutysa.append()
+    '''print(atrybutysa)
     global L
     print(L)
-    czy_znalazlem = False
-    for i in L:
-        if i not in atrybutysa:
-            a.nodes[g]['usluga'] = i
 
-            czy_znalazlem = True
-            break
-    if czy_znalazlem == 0:
-        print("nie mozna tak ustawić uslug")
-        # i tutaj wywoluje pomiejszony zbior galeri
+    for i in list(a.nodes):
+        if i == g:
+            continue'''
 
-    print(G.nodes[g]['usluga'])
-    print(sasiedzi)
+    # czy_znalazlem = False
+    # for i in L:
+    #     if i not in atrybutysa:
+    #         a.nodes[g]['usluga'] = i
+    #         b = a.copy()
+    #         czy_znalazlem = True
+    #         break
+    # if czy_znalazlem == 0:
+    #     print("nie mozna tak ustawić uslug")
+    # i tutaj wywoluje pomiejszony zbior galeri
 
+    '''print(G.nodes[g]['usluga'])
+    print(sasiedzi)'''
     pos = nx.circular_layout(a)
-    nx.draw_networkx_nodes(a, pos, node_size=250)
-    nx.draw_networkx_labels(a, pos)
+    node_labels = nx.get_node_attributes(a, 'usluga')
+    nx.draw_networkx_labels(G, pos, labels=node_labels)
+    nx.draw_networkx_nodes(a, pos, node_size=3000)
+    #nx.draw_networkx_labels(a, pos)
     nx.draw_networkx_edges(a, pos)
     plt.show()
 
-    abezg = a.copy()
-    abezg.remove_node(input("podaj cos do usuniecia"))
-    return usl(abezg, 0)
+usl(G, G)
 
-usl(G, 0)
+
+
+
